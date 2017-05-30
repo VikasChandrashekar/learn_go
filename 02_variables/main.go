6297caa9c0d265cdf7db237ba5499d8037ce6b9c
VikasChandrashekar <vkscs92@gmail.com>
2017-05-29 21:03:14 -0500

    my first commit

---
+++ 02_variables/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 1
+	b := "go language"
+	c := 4.17
+	d := true
+	e := 'M'
+	f := `Do you wanna eat ice cream?`
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+	fmt.Printf("%v \n", e)
+	fmt.Printf("%v \n", f)
+}
